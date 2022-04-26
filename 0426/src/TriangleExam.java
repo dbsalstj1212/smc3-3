e96ceb33014d09547b715c9b1aef2f9702c679b1
dbsalstj1212 <cjejcjejcjejvrj@gmail.com>
2022-04-26 10:20:14 +0900

    0425추가

---
+++ 0426/src/TriangleExam.java
@@ -0,0 +1,23 @@
+
+public class TriangleExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+class Tringle{
+	double bottom;
+	double height;
+	public Triangle(double bt, double hg) {//생성자
+		this.bottom = bt;
+		this.height = hg;
+	}
+	public void setSize(double bt, double hg) {
+		this.bottom = bt;
+		this.height = hg;
+	}
+	
+}
+ 	
+
+}
